d6adf617b449b97c6bdc1235e5b8fb6c1743e9e0
nbgh46 <nbgh46@naver.com>
2021-10-08 15:10:56 +0900

    Initial commit

---
+++ src/main/java/com/example/letmesub/LetMeSubApplication.java
@@ -0,0 +1,20 @@
+package com.example.letmesub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class LetMeSubApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LetMeSubApplication.class, args);
+
+    }
+
+
+
+}
---
+++ src/main/java/com/example/letmesub/controller/PageController.java
@@ -0,0 +1,37 @@
+package com.example.letmesub.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController
+{
+
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login()
+    {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String register()
+    {
+        return "register";
+    }
+
+    @GetMapping("/recommand")
+    public String recommand()
+    {
+        return "recommend.html";
+    }
+
+
+
+
+}
